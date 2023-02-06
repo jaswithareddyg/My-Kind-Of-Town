43499330f7ec4c315cfee29372ff2a10d5de5076
Jas <114607217+jasuchicago@users.noreply.github.com>
2023-02-06 07:36:34 -0600

    Update ViewController.swift

--- My Kind of Town/My Kind of Town/ViewController.swift
+++ My Kind of Town/My Kind of Town/ViewController.swift
@@ -8,12 +8,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func didTapImageView(_ sender: UITapGestureRecognizer) {
+        print("tapped", sender)
+    }
 }
-
