49d9d30aae92488f9ac502e8a75154883d1f274d
Jas <114607217+jasuchicago@users.noreply.github.com>
2023-02-06 04:36:24 -0600

    Create My_Kind_of_TownApp.swift

---
+++ My Kind of Town/My Kind of Town/My_Kind_of_TownApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_Kind_of_TownApp.swift
+//  My Kind of Town
+//
+//  Created by Jaswitha Reddy G on 2/6/23.
+//
+
+import SwiftUI
+
+@main
+struct My_Kind_of_TownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
