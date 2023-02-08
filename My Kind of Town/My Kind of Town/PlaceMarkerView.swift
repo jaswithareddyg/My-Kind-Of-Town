2b2d6014ad15ff52430775046737819a81c5f505
Jas <114607217+jasuchicago@users.noreply.github.com>
2023-02-08 00:52:53 -0600

    Update PlaceMarkerView.swift

--- My Kind of Town/My Kind of Town/PlaceMarkerView.swift
+++ My Kind of Town/My Kind of Town/PlaceMarkerView.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+import MapKit
+
+class PlaceMarkerView: MKMarkerAnnotationView {
+    
+    override var annotation: MKAnnotation? {
+        willSet {
+            clusteringIdentifier = "Place"
+            displayPriority = .defaultLow
+            markerTintColor = .systemRed
+            glyphImage = UIImage(systemName: "pin.fill")
+        }
+    }
+}
