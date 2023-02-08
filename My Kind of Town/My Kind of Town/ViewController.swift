d089ebd4ecc477e645594427138fda6e61311a32
Jas <114607217+jasuchicago@users.noreply.github.com>
2023-02-08 05:10:42 -0600

    Update ViewController.swift

--- My Kind of Town/My Kind of Town/ViewController.swift
+++ My Kind of Town/My Kind of Town/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 import MapKit
 
-class ViewController: UIViewController{
+class ViewController: UIViewController, MKMapViewDelegate{
     
     @IBOutlet var mapView: MKMapView!
     
@@ -23,9 +23,29 @@ class ViewController: UIViewController{
         print("tapped", sender)
     }
     
+    override func viewDidLayoutSubviews() {
+        <#code#>
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.mapView.delegate = self
+        self.mapView.showsCompass = false
+        self.mapView.pointOfInterestFilter = .excludingAll
+        self.mapView.region = DataManager.sharedInstance.loadRegion()!
+        self.mapView.addAnnotations(DataManager.sharedInstance.loadAnnotations()!)
+        self.mapView.register(PlaceMarkerView.self, forAnnotationViewWithReuseIdentifier: MKMapViewDefaultAnnotationViewReuseIdentifier)
         
+        nameLabel.text = DataManager.sharedInstance.loadAnnotations()?.first?.title
+        descriptionLabel.text = DataManager.sharedInstance.loadAnnotations()?.first?.longDescription
+        starButton.isSelected = DataManager.sharedInstance.isFavourite(annotation?.title!)
+    }
+    
+    
+}
+
+extension ViewController: PlacesFavoritesDelegate {
+    func favoritePlace(name: String) {
         
     }
 }
