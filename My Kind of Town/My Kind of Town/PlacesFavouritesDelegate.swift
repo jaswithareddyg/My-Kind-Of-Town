7ce735b5ef350bb0dcf1b838b6d6d58eb7253eba
Jas <114607217+jasuchicago@users.noreply.github.com>
2023-02-08 00:26:50 -0600

    Create PlacesFavouritesDelegate.swift

---
+++ My Kind of Town/My Kind of Town/PlacesFavouritesDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  PlacesFavouritesDelegate.swift
+//  My Kind of Town
+//
+//  Created by Jaswitha Reddy G on 2/8/23.
+//
+
+import Foundation
+import UIKit
+
+class FavouritesViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
