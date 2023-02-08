7a6cc90c57d993039f7cf045de5ddad05f855ecf
Jas <114607217+jasuchicago@users.noreply.github.com>
2023-02-08 00:26:46 -0600

    Create Place.swift

---
+++ My Kind of Town/My Kind of Town/Place.swift
@@ -0,0 +1,14 @@
+//
+//  Place.swift
+//  My Kind of Town
+//
+//  Created by Jaswitha Reddy G on 2/8/23.
+//
+
+import Foundation
+import MapKit
+
+class Place: MKPointAnnotation {
+    var name: String?
+    var longDescription: String?
+}
