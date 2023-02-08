22f354e761b2199296ebf544959926920b389c8c
Jas <114607217+jasuchicago@users.noreply.github.com>
2023-02-08 05:10:46 -0600

    Update DataManager.swift

--- My Kind of Town/My Kind of Town/DataManager.swift
+++ My Kind of Town/My Kind of Town/DataManager.swift
@@ -6,3 +6,55 @@
 //
 
 import Foundation
+import CoreLocation
+import MapKit
+
+public class DataManager {
+    // MARK: - Singleton Stuff
+    public static let sharedInstance = DataManager()
+    
+    //This prevents others from using the default '()' initializer
+    fileprivate init() {}
+    
+    
+    func addFavourite(_ title: String) {
+      
+    }
+    
+    func isFavourite(_ favourtie: String) -> Bool {
+     
+    }
+     
+    func removeAllFavourite() {
+     
+    }
+     
+    func removeFavourite(_ favourite: String) {
+    
+    }
+    
+    func favourites() -> [String] {
+    
+    }
+    
+    //
+    // MARK: - Coordinates
+    //
+    
+    func locationFromName(_ name: String) -> MKCoordinateRegion {
+     
+    }
+     
+    //
+    // MARK: Load Data from PList
+    //
+     
+    func loadRegion() -> MKCoordinateRegion? {
+     
+    }
+     
+    func loadAnnotations() -> [Place]? {
+     
+    }
+    
+}
